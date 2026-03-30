b84e27baaf632f1b163e4e196f87018dfaadaa20
Moeckl-kathrin <kathrin.moeckl@gmail.com>
2026-03-30 13:36:00 +0000

    YOLO commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,7 @@ export default function About() {
           <div className="about__image">
             <div className="about__image-frame">
               <img
-                src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=520&h=640&fit=crop&crop=face"
+                src="https://ik.imagekit.io/1levduhdq/default-image.jpg?updatedAt=1774877551113"
                 alt="Kathrin Möckl"
                 className="about__image-photo"
               />
